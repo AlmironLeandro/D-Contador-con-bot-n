8194c87d18367fe41eab864b69844f38d79d1e9e
AlmironLeandro <leandroealmiron@hotmail.com>
2021-04-07 18:32:22 -0300

    terminado

--- app/src/App.js
+++ app/src/App.js
@@ -3,33 +3,39 @@ import React from 'react';
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer"
 import ItemCount from "./components/ItemCount"
+import {useState, useEffect} from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () =>
-(
-  <div>
-  <NavBar></NavBar>
-  <ItemListContainer greeting="Saludos chicos de coderhouse"></ItemListContainer>
+{
+  //ESTA FUNCION ESTA MAL DEFINIDA YA QUE CADA COMPONENTE TIENE QUE TENER SUS PROPIAS VARIABLES Y FUNCIONES
+  //DE ESTA FORMA SE ESTA COMPARTIENDO Y CAMBIAN TODOS LOS COMPONENTE(VARIABLES) Y SE VUELVEN DEPENDIENTES.
+  const [stockActual, setStockActual] =useState(5);
 
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <ItemCount></ItemCount>
-      </div>
-      <div class="col">
-      <ItemCount></ItemCount>
-      </div>
-      <div class="col">
-      <ItemCount></ItemCount>
-      </div>
-
+  const restarStock = (e, nuevoStock) => 
+  {
+    e.preventDefault();
+    if(stockActual>=nuevoStock){setStockActual((stockActual) => stockActual - nuevoStock);} 
+  }
+return <div>
+    <NavBar></NavBar>
+    <ItemListContainer greeting="Saludos chicos de coderhouse"></ItemListContainer>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <ItemCount stock={stockActual} initial={1} onAdd={restarStock}></ItemCount>
+        </div>
+        <div class="col">
+        <ItemCount stock={stockActual} initial={1} onAdd={restarStock}></ItemCount>
+        </div>
+        <div class="col">
+        <ItemCount stock={stockActual} initial={1} onAdd={restarStock}></ItemCount>
+        </div>
+    </div>
   </div>
-
 </div>
+}
 
-
-</div>
-);
 
 export default App;
--- app/src/components/ItemCount.js
+++ app/src/components/ItemCount.js
@@ -1,30 +1,38 @@
-import {useState} from "react";
-const ItemCount = () => 
+import {useState, useEffect} from "react";
+
+const ItemCount = ({stock,initial,onAdd}) => 
 {
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(initial);
 
     const controlNegative = ()=>{
-        if(counter!==0){setCounter((counter) => counter = counter - 1)}
+        if(counter!=1){setCounter((counter) => counter = counter - 1)}
     }
     
    return <div className="card" style={{width: '18rem'}}>
         <img src="https://http2.mlstatic.com/D_NQ_NP_718161-MLA45132029088_032021-O.jpg" className="card-img-top" alt="..."></img>
         <div className="card-body">
     <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p className="card-text">
+            <li>Boost Clock / Velocidad de memorai</li>
+            <li>1860 MHz / 8 Gbps</li>
+            <li>6GB GDDR5</li>
+            <li>DisplayPort x 3 / HDMI x 1</li>
+        </p>
     
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <button onClick={()=> controlNegative()} type="button" class="btn btn-light">-</button>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                <button onClick={()=> setCounter((counter)=> counter + 1)}  type="button" className="btn btn-light" id="buttonP">+</button>
                 </div>
-                <div class="col">
-                {counter}
+                <div className="col">
+               <p>{counter}</p> 
+               <p>S:{stock}</p>
                     </div>
-                <div class="col">
-                    <button onClick={()=> setCounter((counter)=> counter + 1)}  type="button" class="btn btn-light">+</button>
+                <div className="col">      
+                    <button onClick={()=> controlNegative()} type="button" className="btn btn-light" id="buttonN">-</button>
             </div>
         </div>
+        <button type="button" onClick={(e)=> onAdd(e,counter)} className="btn btn-outline-secondary" >Agregar al carrito</button>
 </div>
     
    
